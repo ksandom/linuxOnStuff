bba1238977580e2e5bf65876903e3b35546f128c
Kevin Sandom <kevinsandom@yahoo.co.uk>
2017-06-02 17:06:32 +0000

    Install: Added installer for Android. HA! My timezone is off by an hour!

---
+++ Android/chroot/generic/install-android.sh
@@ -0,0 +1,33 @@
+#!/system/bin/bash
+# CR - The chroot manager.
+# This script installs CR. View the readme.md for more information.
+#
+# Syntax
+#   ./install.sh [locationToInstall]
+#
+#   locationToInstall defaults to /sdcard, but if you enter something
+#     it will install to the specified location instead.
+# Eg
+#   ./install.sh /sdcard/stuff
+#
+#   Would install stuff like this:
+#     /sdcard/
+#       stuff/
+#         cr          - The thing to run outside the chroot environments.
+#         .cr/
+#           bin       - More useful stuff to run.
+#           config.d  - Where you configure your chroots.
+#           lib       - Code that makes thing go.
+#           mnt       - Where the images get mounted.
+#           state     - For internally tracking the state of each chroot.
+
+
+# Figure out how stuff needs to be installed.
+installLocation=${1:-/sdcard}
+configDir="$installLocation/.cr"
+binPath="$installLocation/cr"
+
+# Get the library to do the grunt work.
+. lib/install.sh
+
+sanityChecks && doInstall
